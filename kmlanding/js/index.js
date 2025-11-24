419b22b2e2877f1bd47ff24bea4ff1b4766cc6d4
apple <apple@appledeMacBook-Pro.local>
2025-11-24 21:49:09 +0800

    push

---
+++ kmlanding/js/index.js
@@ -0,0 +1,17 @@
+new Vue({
+    el:'#app',
+    data:{
+        num:1
+    },
+    methods:{
+        add(){
+            this.num++
+        }
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+})
