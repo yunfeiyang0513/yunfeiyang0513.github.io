0e97a1f091bfbde317056b07f326cadca74cdece
apple <apple@appledeMacBook-Pro.local>
2025-12-04 15:58:50 +0800

    修改scale

--- kmlanding/js/adapt.js
+++ kmlanding/js/adapt.js
@@ -2,8 +2,8 @@ function initRem(size){
     function adapt(size) {
         let html = document.documentElement;
         let wW = html.clientWidth; // 窗口宽度
-        let rem = wW / (size / 100);
-        document.documentElement.style.fontSize = rem + "px";
+        let rem = wW / size ;
+        document.documentElement.style.setProperty('--scale', rem);
     }
     adapt(size)
     window.addEventListener("resize", () => {
